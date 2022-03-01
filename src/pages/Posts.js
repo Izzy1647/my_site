7700c98a3a0e7a20ab96bd4980cc56ddcc4bfc8e
zzy <zhouhoushu@126.com>
2022-03-01 18:56:49 +0000

    add posts

--- src/pages/Posts.js
+++ src/pages/Posts.js
@@ -1,24 +1,19 @@
 import React from 'react'
-import dayjs from 'dayjs'
 import Main from '../layouts/Main'
 import Blog from '../components/Posts/BlogCell'
 import posts from '../data/blog/posts'
 
 const Posts = () => (
   <Main title="Posts" description="My blog posts.">
-    {posts
-      .sort((a, b) => {
-        dayjs(a.date).isAfter(dayjs(b.date)) ? -1 : 1
-      })
-      .map(item => (
-        <Blog
-          data={{
-            title: item.title,
-            date: item.date,
-            desc: item.desc
-          }}
-        />
-      ))}
+    {posts.map(item => (
+      <Blog
+        data={{
+          title: item.title,
+          date: item.date,
+          desc: item.desc
+        }}
+      />
+    ))}
   </Main>
 )
 
